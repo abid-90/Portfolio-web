f4bc45e7ee7e6e043a953622fd499d173fb7d7fd
Abid <sd.abid@gmail.com>
2025-12-06 23:07:28 +0530

    refactor: remove unused dependencies and update middleware
    - Remove body-parser as express now includes built-in middleware
    - Clean up .gitignore by removing unnecessary pic.jpeg
    - Update export syntax in send-email.js to CommonJS

--- api/send-email.js
+++ api/send-email.js
@@ -9,8 +9,7 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-// API handler
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
--- server.js
+++ server.js
@@ -4,15 +4,15 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+ 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static files
 app.use(express.static('.'));
